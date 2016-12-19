d8449f7c6c82aacea520edf6e223694cf01b1eed
kevinbloomquist <rkevinbloom@gmail.com>
2016-12-19 00:09:37 -0700

    end of night still working on exercise 3

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,32 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("exerciseController",exerciseController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Dirty Mike and the Boys";
+  this.age = 38;
+  this.city = "Baltimore";
+  this.state = "MD";
+  this.shout = function($scope){
+  	var word = $scope.toUpperCase;
+  	return word +"!";
+  };
+  
+  // app.filter('shout',function(input){
+  // 	input = input||'';
+  // 	var shouted = input.toUppercase;
+  // 	return shouted +"!";
+  // });
+  };
+
+function exerciseController(){
+	this.class_name = "BNCOZO"
+	this.enrolled_students=10
+	this.start_date = "10/31/2016"
+	this.end_date = "02/03/17"
+	this.daysremaining = 
+
+}
+
