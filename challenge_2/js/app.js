44f1ca24cd4ac7252a6bb446bcf8ad6899a820cd
kevinbloomquist <rkevinbloom@gmail.com>
2016-12-19 18:27:42 -0700

    challenge 2 complete

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -11,10 +11,10 @@ function WelcomeController(){
   this.state = "MD";
 
   // couldn't get either apporach to work
-  this.shout = function(this){
-  	var word = this.toUpperCase;
+  this.shout = function(input){
+  	var word = input.toUpperCase();
   	return word +"!";
-  }
+  };
 };
   
   // app.filter('shout',function(input){
@@ -22,14 +22,25 @@ function WelcomeController(){
   // 	var shouted = input.toUppercase;
   // 	return shouted +"!";
   // });
+  function daysLeft(){
+    var today = new Date();
+    var lastDay = new Date(today.getFullYear()+1,1,3);
+    if(today.getMonth()==1&&today.getDate()>3) {
+      lastDay.setFullYear(lastDay.getFullYear()+1);
+    }
+    var oneDay = 1000*60*60*24;
+    return Math.ceil((lastDay.getTime()- today.getTime())/(oneDay)) + " days left";
   
+  };
 
 function exerciseController(){
-	this.class_name = "BNCOZO"
-	this.enrolled_students=10
-	this.start_date = "10/31/2016"
-	this.end_date = "02/03/17"
-	// this.daysremaining = 
+	this.class_name = "BNCOZO";
+	this.enrolled_students=10;
+	this.start_date = "10/31/2016";
+	this.end_date = "02/03/17";
+	this.daysRemaining = daysLeft();
+
+ 
 
 };
 
